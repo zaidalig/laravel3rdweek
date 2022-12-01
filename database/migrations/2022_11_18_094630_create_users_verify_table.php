060b4fd4675050d552fee4f72399764f25d7813d
Muhammad Zaid Ali <me.mzaidali@gmail.com>
2022-12-01 18:07:06 +0500

    gd added

--- database/migrations/2022_11_18_094630_create_users_verify_table.php
+++ database/migrations/2022_11_18_094630_create_users_verify_table.php
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users_verify');
+        Schema::dropIfExists('verify_users');
     }
 };
